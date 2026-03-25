a4aaa10021803c49d33b695d6ea9848c8d4ead15
tkatkad <mbaktutuk@gmail.com>
2026-03-25 14:26:49 +0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,23 @@
+    <nav>
+      <div class="logo">🎯</div>
+      <div class="menu-toggle" onclick="toggleMenu()">Link SPMB DIY ☰</div>
+      <ul id="nav-links">
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorsleman/">Kalkulator 2025 SMPN Sleman</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorsleman/rekomendasi.html">Rekomendasi 2025 SMPN Sleman</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorkotayogya/">Kalkulator 2025 SMPN Kota Yogyakarta</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorbantul/">Kalkulator 2025 SMPN Bantul</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorsleman/PETUNJUK%20TEKNIS%20PENERIMAAN%20MURID%20BARU%20PADA%20JENJANG%20SMP%20DI%20KAB.SLEMAN%20TAHUN%20AJARAN%202025-2026.pdf">Juknis SMPN Sleman</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorkotayogya/2025kw3405210.pdf">Juknis 2025 SMPN Kota Yogyakarta</a></li>
+        <li><a target="_blank" href="https://vswi.github.io/kalkulatorbantul/Juknis-Bantul-SMPN-keputusan-bupati-2025-226-Juknis-SPMB.pdf">Juknis SMPN Bantul</a></li>
+        <li><a target="_blank" href="https://sleman.spmb.id/020201/hasil">Nilai Jalur Domisili Wilayah Sleman 2025</a></li> 
+		
+		<li><a target="_blank" href="https://sleman.spmb.id/020001/hasil">Jalur Prestasi Sleman Sleman 2025</a></li>
+		
+		<li><a target="_blank" href="https://arsip.spmb.id/2024/sleman/#/020001/statistik">Nilai Terendah 2024 Jalur Prestasi Sleman</a></li>
+        <li><a target="_blank" href="https://arsip.spmb.id/2024/sleman/#/020201/statistik">Nilai Terendah 2024 Jalur Domisili Wilayah Sleman</a></li>
+        <li><a target="_blank" href="https://arsip.spmb.id/2024/yogya/#/020701/statistik">Nilai Terendah 2024 Jalur Prestasi Luar Daerah Kota Yogyakarta</a></li>
+        <li><a target="_blank" href="https://arsip.spmb.id/2024/yogya/#/020301/statistik">Nilai Terendah 2024 Jalur Zonasi Daerah Kota Yogyakarta</a></li>
+        <li><a target="_blank" href="https://arsip.siap-ppdb.com/2024/bantulkab/#/020401/statistik">Nilai Terendah 2024 Jalur Prestasi Bantul</a></li>
+        <li><a target="_blank" href="https://arsip.siap-ppdb.com/2024/bantulkab/#/020001/statistik">Nilai Terendah 2024 Jalur Domisili Wilayah Bantul</a></li>
+      </ul>
+    </nav>
